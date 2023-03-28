64e56f9e95560f2d00d5406bbbb48ec391dafda8
SCIO\diego.renteria <diego.renteria@sciodev.com>
2023-03-28 14:03:28 -0600

    final

--- src/components/article.tsx
+++ src/components/article.tsx
@@ -1,20 +1,53 @@
-interface ArticleProps{
+interface ArticleProps {
     title: string,
     description: string,
     url: string,
-    img:string
+    img: string
 }
 
-function Article({title, description, url, img}:ArticleProps) {
+function Article({ title, description, url, img }: ArticleProps) {
 
+    const card = {
+        width: "20rem",
+        margin: "10px",
+        backgroundColor: "white",
+        borderRadious: "10px",
+        height: "34rem",
+        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+
+    }
+    const cardTitle = {
+        height:"2rem",
+        paddingBottom: "1rem"
+    }
+
+    const cardText = {
+        height: "12rem",
+    }
+
+    const btn={
+        backgroundColor: "#B22D03",
+        border: "none",
+        color: "white",
+        padding: "10px 15px",
+        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.2)",
+        borderRadious: "5px",
+        textDecoration: "none",
+        display: "inline-block",
+        fontSize: "10px",
+    }
+
+    const cardBody={
+        padding:"10px"
+    }
     return (
-        <div className="card" style={{width: "18rem", margin:"10px"}}>
-            <img src={img} className="card-img-top" style={{width: "18rem", height:"12rem"}}/>
-                <div className="card-body">
-                    <h5 className="card-title">{title}</h5>
-                    <p className="card-text">{description}</p>
-                    <a href={url} className="btn btn-primary">See more</a>
-                </div>
+        <div className="card" style={card}>
+            <img src={img} className="card-img-top" style={{ width: "100%", height: "12rem" }} />
+            <div className="card-body" style={cardBody}>
+                <h5 className="card-title" style={cardTitle}>{title}</h5>
+                <p className="card-text" style={cardText}>{description}</p>
+                <a href={url} className="btn" style={btn}>Ver más</a>
+            </div>
         </div>
     );
 }
--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,21 +1,37 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Article from "./article";
+
+
 const Main = () => {
     
     const [articles, setArticles] = useState([]);
+
+    const bodyStyle={
+        backgroundColor: "#FF9877",
+        margin: 0,
+        padding: 0
+    }
+    const title = {
+		border: "1px solid salmon",
+		height: "40px",
+		padding: "10px",
+        marginLeft: "10px",
+        marginRight: "10px",
+        fontFamily: "Serif, Times",
+        fontSize: "30px",
+        backgroundColor: "#CD5C5C",
+  }
     
     useEffect(() => {
-        axios.get<any>('https://newsapi.org/v2/everything?q=covid&language=es&pageSize=20&apiKey=fb180203450147afa51ce92ad38deabd')
+        axios.get<any>('https://newsapi.org/v2/everything?q=covid&language=es&pageSize=30&apiKey=fb180203450147afa51ce92ad38deabd')
         .then((res)=>{
-            setArticles(res.data.articles)
-            // console.log(res.data.articles);
+            setArticles(res.data.articles);
         })
     }, [])
-
     return (
-        <div >
-            <header>New of the day</header>
+        <div style={bodyStyle}>
+            <header style={title}>NOTICIAS COVID</header>
             <div
             style={{
                 display:"flex",
